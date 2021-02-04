049b95383f4ed6686ac30489db3a2ae657274083
Soumyajit Deb <debsoumyajit100@gmail.com>
2021-02-04 11:30:34 +0530

    Add functions to read, create config files and handle case when city is empty

--- weather.py
+++ weather.py
@@ -4,31 +4,77 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+import configparser
+
+
+def check_config_file():
+    """ Checks if current file exists or not"""
+    path = os.path.join('.', '.weather-config')
+    return os.path.isfile(path)
+
+def create_config_file(city):
+    """Creates a config file"""
+    path = os.path.join('.', '.weather-config')
+    with open(path, "w") as f:
+        ret = configparser.ConfigParser()
+        ret.add_section("default")
+        ret.set("default", "city", city)
+        ret.write(f)
+
+
+def read_config_file():
+    """Read a config file and return the config parser object"""
+    path = os.path.join('.', '.weather-config')
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+def create_api(api_key, city):
+    """returns the api with parameters filled in"""
+    api = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city,api_key)
+    return api
 
 def get_current_weather(api_key, city=None):
     """Function to get current weather"""
+
+    response = None
 
     if not api_key:
         raise Exception('ERROR: get_current_weather(): API key is missing')
     
     if not city:
         # get current location
-        pass
+        config = read_config_file()
+        default_city = config['default']['city']
+        api = create_api(api_key, default_city)
+        response = requests.get(api)
+
+        if response.status_code == 404:
+            raise Exception('URL not found\nPlease check your default city name or api key')
     else:
 
-        api = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city,api_key)
+        api = create_api(api_key, city)
         response = requests.get(api)
         # condition to handle failure of requests.
         if response.status_code == 404:
             raise Exception('URL not found\nPlease insert correct city name or api key')
 
-        weather_data = response.json()
-        return weather_data
+    weather_data = response.json()
+    return weather_data
 
 
 if __name__=="__main__":
     load_dotenv()
     api_key = os.getenv('WEATHER_API')
+
+    # check if config file exist
+    existence = check_config_file()
+
+    if not existence:
+        while True:
+            city = input('Enter default city (current permanent location): ')
+            if city: break
+        create_config_file(city)
 
     city = input('Enter city:')
     weather_data = get_current_weather(api_key, city)
