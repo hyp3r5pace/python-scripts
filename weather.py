ed995f77fb26c6467be628f98e1716bab2959a64
Soumyajit Deb <debsoumyajit100@gmail.com>
2021-02-04 11:55:11 +0530

    Refactor code

--- weather.py
+++ weather.py
@@ -62,8 +62,23 @@ def get_current_weather(api_key, city=None):
     weather_data = response.json()
     return weather_data
 
+def show_current_temperature(weather_data, city):
+    """Print the current temperature on terminal"""
 
-if __name__=="__main__":
+    # temperature converted to celsius
+    avg_temp = weather_data['main']['temp']
+    avg_temp = avg_temp - 273.15
+    min_temp = weather_data['main']['temp_min'] - 273.15
+    max_temp = weather_data['main']['temp_max'] - 273.15
+
+    print("{} TEMPERATURE STATS\n".format(city.upper()))
+    # temperature rounded to 2 decimal places.
+    print('Average Temperature: {} celsius'.format(round(avg_temp, 2)))
+    print('Maximum Temperature: {} celsius'.format(round(max_temp, 2)))
+    print('Minimum Temperature: {} celsius'.format(round(min_temp, 2)))
+
+
+def main():
     load_dotenv()
     api_key = os.getenv('WEATHER_API')
 
@@ -78,14 +93,10 @@ if __name__=="__main__":
 
     city = input('Enter city:')
     weather_data = get_current_weather(api_key, city)
-    # temperature converted to celsius
-    avg_temp = weather_data['main']['temp']
-    avg_temp = avg_temp - 273.15
-    min_temp = weather_data['main']['temp_min'] - 273.15
-    max_temp = weather_data['main']['temp_max'] - 273.15
 
-    print("{} TEMPERATURE STATS\n".format(city.upper()))
-    # temperature rounded to 2 decimal places.
-    print('Average Temperature: {} celsius'.format(round(avg_temp, 2)))
-    print('Maximum Temperature: {} celsius'.format(round(max_temp, 2)))
-    print('Minimum Temperature: {} celsius'.format(round(min_temp, 2)))
+    # print weather info on terminal
+    show_current_temperature(weather_data, city)
+
+
+if __name__=="__main__":
+    main()
