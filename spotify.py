9e6f3568ebcec7800a7292dcc26fd2c5acd55237
Soumyajit Deb <debsoumyajit100@gmail.com>
2021-03-02 10:49:56 +0530

    Remove limit parameter from listPlaylist() function

--- spotify.py
+++ spotify.py
@@ -6,15 +6,16 @@ import os
 from dotenv import load_dotenv
 import argparse
 
-def listPlaylist(limit=None):
+def listPlaylist():
     """ Function to list the playlist of a certain user"""
     # define the scope of this function on the user's spotify account data
     scope = 'playlist-read-private'
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    results = sp.current_user_playlists(limit)
+    results = sp.current_user_playlists()
     print("Your Spotify playlist \n")
     for i,item in enumerate(results['items']):
         print("%d %s" %(i, item['name']))
+
 
 def main(args):
     # setting the environment variable by reading from key value pair from .env file
