004de0213891f5435d97195b1233675def70239e
Soumyajit Deb <debsoumyajit100@gmail.com>
2021-03-02 10:22:28 +0530

    Add spotify command and playlist argument to display user's list of playlist

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 pip3 install -U python-dotenv
+pip3 install spotipy
---
+++ spotify.py
@@ -0,0 +1,25 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+import json
+import os
+from dotenv import load_dotenv
+import argparse
+
+def listPlaylist(limit=None):
+    """ Function to list the playlist of a certain user"""
+    # define the scope of this function on the user's spotify account data
+    scope = 'playlist-read-private'
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    results = sp.current_user_playlists(limit)
+    print("Your Spotify playlist \n")
+    for i,item in enumerate(results['items']):
+        print("%d %s" %(i, item['name']))
+
+def main(args):
+    # setting the environment variable by reading from key value pair from .env file
+    load_dotenv()
+    if args.playlist:   listPlaylist()
+
+if __name__ == "__main__":
+    main()
